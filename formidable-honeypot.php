0aa89d1f79c38629c8b832bc759b8b2c47a1fe8d
Evan Mattson <evan.mattson@tunatraffic.com>
2016-11-09 22:54:31 +0200

    refactor
    - fixes notice about undefined index ‘form_id’
    - much cleaner

--- formidable-honeypot.php
+++ formidable-honeypot.php
@@ -7,8 +7,7 @@ Author: Ryan Pilling
 Version: 0.2
 Author URI: http://wpbiz.co/
 */
-	
-// Define our plugin's wrapper class
+
 if ( !class_exists( "WPBizFrmHoneypot" ) )
 {
 	class WPBizFrmHoneypot
@@ -35,69 +34,74 @@ if ( !class_exists( "WPBizFrmHoneypot" ) )
 		
 		function notice_missing_formidable(){
 			?>
-		    <div class="error">
-		        <p><?php _e( 'Formidable Not Installed! The plugin "Formidable Honeypot by WPbiz.co" does not work without the Formidable plugin.', 'wpbiz-frm-honeypot' ); ?></p>
-		    </div>
-		    <?php
+			<div class="error">
+				<p><?php _e( 'Formidable Not Installed! The plugin "Formidable Honeypot by WPbiz.co" does not work without the Formidable plugin.', 'wpbiz-frm-honeypot' ); ?></p>
+			</div>
+			<?php
 		}
 
-		function add_honeypot($form, $action='', $errors=''){
-		    //insert honeypot
-		    global $frm_next_page, $frm_vars;
+		function add_honeypot($form, $action='', $errors = array()) {
+			global $frm_next_page, $frm_vars;
 
-		    // skip captcha if user is logged in
-			if ( (is_admin() and !defined('DOING_AJAX')) or ( is_user_logged_in() ))
-			    return;
-			   
-			//skip if there are more pages for this form  
-			if( ( is_array($errors) and !isset( $errors['hnypt'] ) ) or (is_array($frm_vars) and isset($frm_vars['next_page']) and isset($frm_vars['next_page'][$form->id])) or (is_array($frm_next_page) and isset($frm_next_page[$form->id])))
+			// Skip captcha if user is logged in
+			if ((is_admin() && !defined('DOING_AJAX')) || is_user_logged_in()) {
 				return;
+			}
+
+			// Skip if there are more pages for this form
+			if (! isset($errors['hnypt']) || isset($frm_vars['next_page'][$form->id]) || isset($frm_next_page[$form->id])) {
+				return;
+			}
 
 			// captcha html
-			echo '<div id="frm_field_hnypt" class="form-field  frm_top_container" style="display: none;">';
-				
-				echo '<label class="frm_primary_label">'. _e( 'Please leave this blank', 'wpbiz-frm-honeypot' ) .'</label>';
+			$captcha_label = __('Please leave this blank', 'wpbiz-frm-honeypot');
 
-		    	echo '<input type="text" name="firstname_hnypt" id="firstname_hnypt" value="" />';
-		
+			echo <<<HTML
+<div id="frm_field_hnypt" class="form-field frm_top_container" style="display: none;">
+	<label class="frm_primary_label">$captcha_label</label>
+	<input type="text" name="firstname_hnypt" id="firstname_hnypt" value="" />
+</div>
+HTML;
 
-			echo '</div>';
-
-			if( is_array($errors) and isset( $errors['hnypt'] ) )
+			if (is_array($errors) && isset($errors['hnypt'])) {
 				echo '<div class="frm_error">'. $errors['hnypt'] .'</div>';
-
-
+			}
 		}
 
-		function validate_honeypot($errors, $values=''){
+		function validate_honeypot($errors, $values = array()) {
 
-			// skip hpneypot if user is logged in and the settings allow
-			if ( (is_admin() and !defined('DOING_AJAX')) or is_user_logged_in() )
-				return $errors;
-		        
-		    //don't require if editing
-		    $action_var = isset($_REQUEST['frm_action']) ? 'frm_action' : 'action';
-		  	if(isset($values[$action_var]) and $values[$action_var] == 'update')
-		  		return $errors;
-		  	unset($action_var);
-		  	
-		  	//don't require if not on the last page
 			global $frm_next_page, $frm_vars;
-			if((is_array($frm_vars) and isset($frm_vars['next_page']) and isset($frm_vars['next_page'][$values['form_id']])) or (is_array($frm_next_page) and isset($frm_next_page[$values['form_id']])))
+
+			$form_id = isset($values['form_id']) ? $values['form_id'] : false;
+
+			// Skip honeypot if user is logged in and the settings allow
+			if ((is_admin() && ! defined('DOING_AJAX')) || is_user_logged_in()) {
 				return $errors;
-		  		
-		  	//if the honeypot wasn't incuded on the page
-			if(!isset($_POST['firstname_hnypt'])){
-		        return $errors;
-		    }
-		  	
+			}
 
-		  	// If captcha not complete, return error
-		  	if ( $_POST['firstname_hnypt'] != '' ) 	
-		  		$errors['hnypt'] = __( 'Due to suspected SPAM, this form was not submitted. Please do not use auto-fill.', 'wpbiz-frm-honeypot' );
-		  			  
+			// Don't require if editing
+			$action_var = isset($_REQUEST['frm_action']) ? 'frm_action' : 'action';
 
-		    return $errors;
+			if (isset($values[$action_var]) && $values[$action_var] == 'update') {
+				return $errors;
+			}
+
+			// Don't require if not on the last page
+			if (isset($frm_vars['next_page'][$form_id]) || isset($frm_next_page[$form_id])) {
+				return $errors;
+			}
+
+			// If the honeypot wasn't included on the page
+			if (! isset($_POST['firstname_hnypt'])) {
+				return $errors;
+			}
+
+			// If captcha not complete, return error
+			if (! empty($_POST['firstname_hnypt'])) {
+				$errors['hnypt'] = __('Due to suspected SPAM, this form was not submitted. Please do not use auto-fill.', 'wpbiz-frm-honeypot');
+			}
+
+			return $errors;
 		}
 	}
 } // End Class
